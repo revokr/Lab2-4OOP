929cc80a373bab62fc7027a3d46943e76cec244e
revokr <101871749+revokr@users.noreply.github.com>
2024-03-24 19:59:17 +0200

    Create tests.h

---
+++ tests/tests.h
@@ -0,0 +1,25 @@
+
+/*
+	Tests the creation of a product
+*/
+void teste_create_product();
+
+/*
+	Tests the deletion of a product
+*/
+void teste_delete_product();
+
+/*
+	Tests the validation of a product
+*/
+void teste_validator();
+
+/*
+	Tests the repository
+*/
+void teste_repo();
+
+/*
+	Tests the service
+*/
+void teste_service();
